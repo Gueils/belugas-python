1b8f0ce361d23642cdf88ea50b15294b4b99bc54
Abraham Kuri <kurenn@icalialabs.com>
2017-01-05 14:51:34 -0600

    Adds django to the dependency dictionary

--- lib/belugas/python/standard_names/base.rb
+++ lib/belugas/python/standard_names/base.rb
@@ -2,6 +2,8 @@ module Belugas
   module Python
     module StandardNames
       NAMES = {
+        "Django" => { "standard_name" => "django",
+                                   "categories" => ["Framework"] },
         "django-postgrespool" => { "standard_name" => "postgresql",
                                    "categories" => ["Database"] }
       }
