e9967dfb4c8e8b9a771533e5427bd773942c9878
Luis Ignacio <thelastinuit@users.noreply.github.com>
2017-03-13 19:44:23 -0600

    Update patterns.rb

--- lib/belugas/python/parser/patterns.rb
+++ lib/belugas/python/parser/patterns.rb
@@ -2,7 +2,7 @@ module Belugas
   module Python
     module Parser
       module Patterns
-        REQUIREMENT_CALL =/(?<name>(.*))==(?<version>(\d.\d.\d))|(?<name>(.*))>=(?<version>(\d.\d.\d))|(?<name>(.*))>(?<version>(\d.\d.\d))|(?<name>(.*))~=(?<version>(\d.\d.\d))/
+        REQUIREMENT_CALL =/(?<name>(.*))==(?<version>(\d.\d))|(?<name>(.*))>=(?<version>(\d.\d))|(?<name>(.*))>(?<version>(\d.\d))|(?<name>(.*))~=(?<version>(\d.\d))/
       end
     end
   end
