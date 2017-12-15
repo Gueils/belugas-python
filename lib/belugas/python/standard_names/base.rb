71c4c59ed947a2647b67499ac056d7753463270d
Eduardo Herrera <lalo@icalialabs.com>
2017-12-14 19:56:27 -0600

    Fix for hound A bone to the dog

--- lib/belugas/python/standard_names/base.rb
+++ lib/belugas/python/standard_names/base.rb
@@ -2,37 +2,37 @@ module Belugas
   module Python
     module StandardNames
       NAMES = {
-        "django" => {
-          "standard_name" => "django",
-          "categories" => ["Framework"]
+        'django' => {
+          'standard_name' => 'django',
+          'categories' => ['Framework']
         },
-        "flask" => {
-          "standard_name" => "flask",
-          "categories" => ["Framework"]
+        'flask' => {
+          'standard_name' => 'flask',
+          'categories' => ['Framework']
         },
-        "django-postgrespool" => {
-          "standard_name" => "postgresql",
-          "categories" => ["Database"]
+        'django-postgrespool' => {
+          'standard_name' => 'postgresql',
+          'categories' => ['Database']
         },
-        "psycopg2" => {
-          "standard_name" => "postgresql",
-          "categories" => ["Database"],
-          "version" => "9"
+        'psycopg2' => {
+          'standard_name' => 'postgresql',
+          'categories' => ['Database'],
+          'version' => '9'
         },
-        "mysql-python" => {
-          "standard_name" => "mysql",
-          "categories" => ["Database"],
-          "version" => "5"
+        'mysql-python' => {
+          'standard_name' => 'mysql',
+          'categories' => ['Database'],
+          'version' => '5'
         },
-         "mysqlclient" => {
-           "standard_name" => "mysql",
-           "categories" => ["Database"],
-           "version" => "5"
+         'mysqlclient' => {
+           'standard_name' => 'mysql',
+           'categories' => ['Database'],
+           'version' => '5'
          },
-           "celery" => {
-             "standard_name" => "celery",
-             "categories" => ["Service"],
-             "version" => "4.1"
+           'celery' => {
+             'standard_name' => 'celery',
+             'categories' => ['Service'],
+             'version' => '4.1'
         }
       }
     end
